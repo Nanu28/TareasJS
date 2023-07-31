867ad606bfc519c6fe8f7f2861e560515bb43037
Nanu28 <na.aliste@live.cl>
2023-07-31 07:48:58 -0400

    ejercicio1

---
+++ ejercicio1AlisteNadia.js
@@ -0,0 +1,48 @@
+let miNombre= "Nadia"
+let miApellido= "Aliste"
+let miEdad= 33
+let miMascota="Berlin"
+let edadMascota= 5
+// Visualizar todas las variables dentro de la consola del navegador escribiendo el nombre de cada una de ellas.
+let nombreCompleto= miNombre + " " + miApellido
+let textoPresentacion = miNombre + " " + miApellido + " " + miEdad + " " + miMascota + " " + edadMascota
+let sumaEdades= miEdad + edadMascota
+let restarEdades= miEdad - edadMascota
+let productoEdades= miEdad * edadMascota
+let divisionEdades= miEdad / edadMascota
+
+let alumno = {
+    nombre: "Andres",
+    edad: 41,
+    nacionalidad: "chileno",
+    cohort: "51",
+    especialidad: "Java"
+
+}
+
+let mascota = {
+    nombre: "Idoia",
+    edad: 8,
+    color: "castaño",
+    raza: "Yorkshire",
+    peso: "2 kilos"
+    
+}
+
+let frutas = ["manzana", "banana","anana","naranja","sandia"]
+//ejercicio 13 no entender//
+
+// 14
+let numeros= [1,2,3,4,5]
+
+//15
+let familia= ["papá", "mamá", "hijo","hija", "perro","gato"]
+
+//16
+let textoAleatorio= [{frutas (2)} + {numeros (4)} + {familia (5)}]?
+
+//17?
+
+//18?
+
+//19?
